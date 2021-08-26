eb600228a7e15efe5d5ff3e0e0822e1e49ceb9bd
no-bash <blunskay@gmail.com>
2021-08-26 15:48:17 +0400

    init

---
+++ main.js
@@ -0,0 +1,19 @@
+
+var userAgent = navigator.userAgent || navigator.vendor;
+var url;
+
+if (/ios/i.test(userAgent))
+{
+    url = 'https://apps.apple.com/us/app/dungeon-crusher-afk-heroes/id1465892600'
+    
+}
+else
+{
+    url = 'https://play.google.com/store/apps/details?id=com.towardsmars.dungeoncrusher'
+}
+console.log(url);
+
+let storeBtn = document.getElementById("store-btn")
+storeBtn.addEventListener('click', ()=>{
+    window.location.href = url
+})
